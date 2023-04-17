59735d2cb7f48a61336ef38d886d03b9049533d2
Dmitrii <dima12sik@yandex.ru>
2023-04-17 17:49:23 +0300

    updated channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,11 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+# Объявление глобальных переменных
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_DATA_V3_API_KEY')
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
 
 class Channel:
@@ -5,8 +13,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        if not isinstance(channel_id, str):
+            raise ValueError('chanel_id should be string')
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        info = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(info, ensure_ascii=False, indent=2))
